2f570601fd8548e29c23990e750013788bdb7589
aEnchev669 <aenchev669@abv.bg>
2022-09-19 17:58:53 +0300

    Stacks and Queues - Exercise

---
+++ Stacks and Queues - Exercise/01. Basic Stack Operations/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nsx = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int numOfElements = nsx[0];
+            int numsToPop = nsx[1];
+            int numToCheck = nsx[2];
+
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> numbers = new Stack<int>(nums);
+            Stack<int> finalNums = new Stack<int>();
+            int smallestNum = int.MaxValue;
+
+            int count = numbers.Count;
+            for (int i = 0; i < numsToPop; i++)
+            {
+                numbers.Pop();
+            }
+
+            if (numbers.Count <= 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                for (int i = 0; i < count - numsToPop; i++)
+                {
+                    int check = numbers.Peek();
+                    if (check == numToCheck)
+                    {
+                        Console.WriteLine("true");
+                        return;
+                    }
+                    else
+                    {
+                        finalNums.Push(numbers.Pop());
+                    }
+                }
+                for (int i = 0; i < count - numsToPop; i++)
+                {
+                    if (smallestNum > finalNums.Peek())
+                    {
+                        smallestNum = finalNums.Pop();
+                    }
+                }
+                Console.WriteLine(smallestNum);
+            }
+        }
+    }
+}
---
+++ Stacks and Queues - Exercise/02. Basic Queue Operations/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nsx = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int countOfNumbers = nsx[0];
+            int numsToPop = nsx[1];
+            int numToCheck = nsx[2];
+
+            Queue<int> numbers = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            
+            for (int i = 0; i < numsToPop; i++)
+            {
+                numbers.Dequeue();
+            }
+
+            int count = numbers.Count;
+            if (numbers.Count <= 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    if (numToCheck == numbers.Peek())
+                    {
+                        Console.WriteLine("true");
+                        return;
+                    }
+                    else
+                    {
+                        numbers.Enqueue(numbers.Dequeue());
+                    }
+                }
+                int minNum = int.MaxValue;
+                for (int i = 0; i < count; i++)
+                {
+                    if (minNum > numbers.Peek())
+                    {
+                        minNum = numbers.Dequeue();
+                    }
+                }
+                Console.WriteLine(minNum);
+            }
+        }
+    }
+}
